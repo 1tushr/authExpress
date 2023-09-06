f3a9d7b294e66552ca2ebb173ad0140d84c61f73
Tushar Verma <tuhsar2022@gmail.com>
2023-09-06 09:31:04 +0530

    used JWt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,22 @@
-const User =require( "../models/users");
-exports.signup= async(req,res)=>{
-    const newUser=await User.create(req.body);
-    res.status(201).json({
-        status:'success',
-        data:{
-            user:newUser
-        }
-    });
+const User = require("../models/users");
+const jwt = require("jsonwebtoken");
+exports.signup = async (req, res) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+      token:token
+    },
+  });
 };
-
-
--- models/users.js
+++ models/users.js
@@ -35,7 +35,7 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
-  next();
+  next(); 
 });
 
 const User = mongoose.model("User", userSchema);
